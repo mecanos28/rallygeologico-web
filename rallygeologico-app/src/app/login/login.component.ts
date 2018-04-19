a381e6609667a27d0aebfe5c6e0a120af4c256a1
mecanos28 <31737854+mecanos28@users.noreply.github.com>
2018-04-18 21:43:23 -0600

    Template register and login logic, added ngx-facebook support to project

--- rallygeologico-app/src/app/login/login.component.ts
+++ rallygeologico-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FacebookService, InitParams, LoginOptions, LoginResponse, AuthResponse} from 'ngx-facebook';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,129 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username:string;
+  password:string;
+  error:string;
+  fbId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  fbToken: string;
+  loginWithFacebook:boolean=false;
+
+
+
+  constructor(private fb: FacebookService){
+    console.log('Initializing Facebook');
+    let initParams: InitParams = {
+      appId: '785307394989604',
+      xfbml: true,
+      version: 'v2.11'
+    };
+    fb.init(initParams);
+    console.log('Initialized Facebook');
+
+    /*this.studentService.isLoggedIn().then((user: Student) => {
+     this.userDataService.updateStudent(user);
+     this.router.navigate(['/dashboard']);
+     })*/
+  }
+  onLogin(){
+    if (this.username && this.password){
+      this.studentService.login(this.username,this.password).then((user: boolean)=>{
+        this.error = null;
+        if(user)
+          this.studentService.isLoggedIn().then((user: Student) => {
+            this.userDataService.updateStudent(user);
+            if (user.passwordNeedsChange){
+              this.router.navigate(['/change/' + user.id]);
+            } else {
+              this.router.navigate(['/dashboard']);
+            }
+          })
+      }).catch( reason => {
+        this.error = "The email or password are incorrect";
+      });
+    } else {
+      this.error = "Some required inputs are missing";
+    }
+  }
+
+  //Login if the Enter Key is pressed
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (event.keyCode === 13) {
+      if (this.username && this.password){
+        this.onLogin();
+      }
+    }
+  }
+
+
+
+  loginWithOptions() {
+    const loginOptions: LoginOptions = {
+      enable_profile_selector: true,
+      return_scopes: true,
+      scope: 'public_profile,email'
+    };
+
+    this.fb.login(loginOptions)
+      .then((res: LoginResponse) => {
+        this.loginWithFacebook = true;
+        console.log('Logged in', res);
+        this.fb.api('me?fields=id,first_name,last_name,email')
+          .then((res: any) => {
+            console.log('Got the users profile information'+ res);
+            this.fbId = res.id;
+            this.firstName = res.first_name;
+            this.lastName = res.last_name;
+            this.email = res.email;
+            this.fbToken = this.fb.getAuthResponse().accessToken;
+            console.log("Login got : "+this.fbId +" "+this.firstName +" "+ this.lastName +" "+this.email+" "+this.fbToken);
+            this.fbLogin();
+          })
+          .catch(this.handleErrorProfile);
+      })
+      .catch(this.handleErrorLogin);
+  }
+
+
+  fbLogin(){
+    this.studentService.loginWithFacebook(this.fbId,this.fbToken).then((authentication: boolean)=>{
+      this.error = null;
+      if(authentication){
+        this.studentService.isLoggedIn().then((user: Student) => {
+          this.userDataService.updateStudent(user);
+          this.router.navigate(['/dashboard']);
+        })
+      }
+
+    }).catch( reason => {
+      this.error = "Unable to login with Facebook.";
+    });
+  }
+
+  private handleErrorLogin(error) {
+    console.error('Error processing FB login', error);
+  }
+
+  /**
+   * Obtains the user's information from his facebook profile and stores it.
+   */
+  getProfile() {
+
+  }
+
+  private handleErrorProfile(error) {
+    console.error('Error processing FB profile', error);
+  }
+
+  getLoginStatus() {
+    this.fb.getLoginStatus()
+      .then(console.log.bind(console))
+      .catch(console.error.bind(console));
+  }
 
   ngOnInit() {
   }
--- rallygeologico-app/src/app/register/register.component.ts
+++ rallygeologico-app/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FacebookService, InitParams, LoginOptions, LoginResponse, AuthResponse} from 'ngx-facebook';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,155 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  fbId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  password2:string;
+  confirmation:boolean;
+  levelId:string = "no";
+  levels: Level[];
+  error:string;
+  registerWithFacebook:boolean = false;
+
+  constructor(private levelService: LevelService, private studentService: UserService, private router: Router, private fb: FacebookService) {
+    console.log('Initializing Facebook');
+    let initParams: InitParams = {
+      appId: '785307394989604',
+      xfbml: true,
+      version: 'v2.11'
+    };
+    fb.init(initParams);
+    console.log('Initialized Facebook');
+
+
+
+    this.levelService.availableLevel().then((levels: Level[]) =>{
+      if(levels){
+        this.levels = [];
+        this.levels = levels;
+      }
+    });
+
+
+  }
+
 
   ngOnInit() {
   }
+
+
+  seleted_level(value){
+    this.levelId= value;
+  }
+
+  facebookRegisterService(){
+    this.studentService.signUpWithFacebook(this.fbId, this.email, this.firstName, this.lastName, this.levelId).then((user: Student) => {
+      if (user) {
+        this.router.navigate(['/login']);
+      }
+      console.log(user);
+    });
+  }
+
+  create_acount(){
+    if (this.registerWithFacebook){
+      this.facebookRegisterService();
+    }
+    else {
+      if (this.firstName && this.lastName && this.email && this.password && this.password2 && this.confirmation && this.levelId != "no") {
+        if (this.password.localeCompare(this.password2) == 0) {
+          this.error = null;
+          this.studentService.signUp(this.email, this.firstName, this.lastName, this.password, this.levelId).then((user: Student) => {
+            if (user) {
+              this.router.navigate(['/login']);
+            }
+          });
+        } else {
+          this.error = "Passwords do not match";
+        }
+      } else {
+        this.error = "Some required inputs are missing";
+      }
+    }
+
+  }
+
+  match_passwords(){
+    if (this.password && this.password2){
+      return (this.password.localeCompare(this.password2) == 0);
+    } else {
+      return true;
+    }
+
+  }
+
+  invalid_level(){
+    //return (this.levelId=="no"); use this once levels are available
+
+    return false;
+  }
+
+  isFacebookRegister() {
+    return this.registerWithFacebook;
+  }
+
+  loginWithOptions() {
+
+    const loginOptions: LoginOptions = {
+      enable_profile_selector: true,
+      return_scopes: true,
+      scope: 'public_profile,email'
+    };
+
+    this.fb.login(loginOptions)
+      .then((res: LoginResponse) => {
+        console.log('Logged in', res);
+        this.getProfile();
+        this.registerWithFacebook = true;
+      })
+      .catch(this.handleErrorLogin);
+
+
+
+
+
+
+
+  }
+
+  private handleErrorLogin(error) {
+    console.error('Error processing FB login', error);
+  }
+
+  /**
+   * Obtains the user's information from his facebook profile and stores it.
+   */
+  getProfile() {
+    this.fb.api('me?fields=id,first_name,last_name,email')
+      .then((res: any) => {
+        console.log('Got the users profile information'+ res);
+        this.fbId = res.id;
+        this.firstName = res.first_name;
+        this.lastName = res.last_name;
+        this.email = res.email;
+        console.log(this.fbId +" "+this.firstName +" "+ this.lastName +" "+this.email);
+        return res;
+
+      })
+      .catch(this.handleErrorProfile);
+  }
+
+  private handleErrorProfile(error) {
+    console.error('Error processing FB profile', error);
+  }
+
+  getLoginStatus() {
+    this.fb.getLoginStatus()
+      .then(console.log.bind(console))
+      .catch(console.error.bind(console));
+  }
+
 
 }
