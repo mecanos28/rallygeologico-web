1fe96761c4a4d937a1284e9f567347bac02b1e70
carlos-gamboa <cali1997gv@hotmail.com>
2018-04-25 13:35:21 -0600

    Invitation get all users

--- rallygeologico-app/src/app/user/competition/competition.component.ts
+++ rallygeologico-app/src/app/user/competition/competition.component.ts
@@ -10,17 +10,55 @@ import {User} from "../../model/user";
 })
 export class CompetitionComponent implements OnInit {
 
-  user: User;
-  users: User[];
+    pageSize : number = 10;
+    currentPage : number = 0;
+    totalUsers : number = 0;
 
-  constructor(private userService: UserService, private dataService: DataService) {
-    this.user = this.dataService.getUser();
-    this.userService.getUsers().subscribe((users: User[]) => {
-      this.users = users;
-    });
-  }
+    searchQuery : string = "";
 
-  ngOnInit() {
-  }
+    user: User;
+    users : User[];
+    allUsers: User[];
+    showedUsers: User[];
+
+    constructor(private userService: UserService, private dataService: DataService) {
+        this.user = this.dataService.getUser();
+        this.userService.getUsers().subscribe((users: User[]) => {
+          this.allUsers = users;
+          this.reloadUsers(users);
+        });
+    }
+
+    reloadUsers(users : User[]) : void{
+        this.users = users;
+        this.totalUsers = users.length;
+        this.showedUsers = users.slice(0, this.pageSize);
+        this.currentPage = 0;
+    }
+
+    pageChange() : void{
+        if(this.users) {
+            this.showedUsers = this.users.slice((this.currentPage - 1) * this.pageSize, ((this.currentPage) * this.pageSize));
+        }
+    }
+
+    searchUser(){
+        let usersToShow = [];
+
+        if(this.searchQuery.length >= 1) {
+            for (let user of this.users) {
+                if (user.Username.toLowerCase().startsWith(this.searchQuery.toLowerCase())) {
+                    usersToShow.push(user);
+                }
+            }
+            this.reloadUsers(usersToShow);
+        }else{
+            this.reloadUsers(this.allUsers);
+        }
+    }
+
+    ngOnInit() {
+
+    }
 
 }
